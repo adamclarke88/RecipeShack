413ae7b362906c134dc5b12d2e29de32511b879e
Adam Clarke <adamclarke88@gmail.com>
2019-02-06 15:04:19 +0000

    Created RecipeShack project

---
+++ RecipeShack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RecipeShack.MvcApplication" Language="C#" %>
---
+++ RecipeShack/Scripts/Site.js
@@ -0,0 +1,12 @@
+﻿//$(".nav a").on("click", function () {
+//    $(".nav").find(".active").removeClass(".active");
+//    $(this).parent().addClass("active");
+//});
+
+//$(document).on('click', '.nav li', function (e) {
+//    $(this).addClass('active').siblings().removeClass('.active');
+//});
+
+$(".nav li").click(function () {
+    console.log("clicked on nav link");
+});
